e6fd2ab75966ac5a48945087f51304b2576c8790
jakubweglowski <jw430620@students.mimuw.edu.pl>
2025-01-30 13:26:08 +0100

    Minor changes to LSTModel

--- LSTModel/exp.py
+++ LSTModel/exp.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import numpy as np
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
+from LSTModel.LSTM import LSTM
 
 class TimeSeriesDataset(Dataset):
     def __init__(self, X, y):
