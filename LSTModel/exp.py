71b65910853cc5b3b87722f32faac6d2a456d1ba
kussy29 <121519986+kussy29@users.noreply.github.com>
2025-01-30 11:50:37 +0100

    LSTM oraz jego uczenie

---
+++ LSTModel/exp.py
@@ -0,0 +1,89 @@
+import torch
+import torch.nn as nn
+import numpy as np
+from LSTM import LSTM
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+
+class TimeSeriesDataset(Dataset):
+    def __init__(self, X, y):
+        self.X = X
+        self.y = y
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, i):
+        return self.X[i], self.y[i]
+
+
+class Exp:
+    
+    def __init__(self,input_size, hidden_size, num_stacked_layers,X_train,y_train,X_test,y_test,
+                 device='cpu',learning_rate=0.0001,num_epochs=20,batch_size=16):
+        self.input_size=input_size
+        self.hidden_size=hidden_size
+        self.num_stacked_layers=num_stacked_layers
+        self.device=device
+        
+        self.model=LSTM(input_size, hidden_size, num_stacked_layers).to(self.device)
+        
+        self.learning_rate = learning_rate
+        self.num_epochs = num_epochs
+        self.loss_function = nn.MSELoss()
+        self.optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+        
+        self.train_dataset = TimeSeriesDataset(X_train, y_train)
+        self.test_dataset = TimeSeriesDataset(X_test, y_test)
+
+        self.train_loader = DataLoader(self.train_dataset, batch_size=batch_size, shuffle=True)
+        self.test_loader = DataLoader(self.test_dataset, batch_size=batch_size, shuffle=False)
+        
+    def train_one_epoch(self):
+        self.model.train(True)
+        print(f'Epoch: {epoch + 1}')
+        running_loss = 0.0
+
+        for batch_index, batch in enumerate(self.train_loader):
+            x_batch, y_batch = batch[0].to(self.device), batch[1].to(self.device)
+
+            output = self.model(x_batch)
+            loss = self.loss_function(output, y_batch)
+            running_loss += loss.item()
+
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+
+            if batch_index % 100 == 99:  # print every 100 batches
+                avg_loss_across_batches = running_loss / 100
+                print('Batch {0}, Loss: {1:.3f}'.format(batch_index+1,
+                                                    avg_loss_across_batches))
+                running_loss = 0.0
+        print()
+       
+    def validate_one_epoch(self):
+        self.model.train(False)
+        running_loss = 0.0
+
+        for batch_index, batch in enumerate(self.test_loader):
+            x_batch, y_batch = batch[0].to(self.device), batch[1].to(self.device)
+
+            with torch.no_grad():
+                output = self.model(x_batch)
+                loss = self.loss_function(output, y_batch)
+                running_loss += loss.item()
+
+        avg_loss_across_batches = running_loss / len(test_loader)
+
+        print('Val Loss: {0:.3f}'.format(avg_loss_across_batches))
+        print('***************************************************')
+        print() 
+        
+    def train(self):
+        for epoch in range(self.num_epochs):
+            train_one_epoch(self)
+            validate_one_epoch(self)
+        
+        return self.model
+    
