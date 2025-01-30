f5bcfac03bbdb4d3a2a3127ce9272231e028508c
jakubweglowski <jw430620@students.mimuw.edu.pl>
2025-01-30 10:41:26 +0100

    Advances in TrendAnalysis module: MA

--- TechFunctions/StatisticalFunctions.py
+++ TechFunctions/StatisticalFunctions.py
@@ -4,3 +4,6 @@ import numpy as np
 def EMA(y: pd.Series, alpha: float):
     weights = np.array([(1-alpha)**(len(y)-i+1) for i in range(len(y))])
     return (y.values).dot(weights)/np.sum(weights)
+
+def SMA(y: pd.Series):
+    return y.mean()
--- TrendAnalysis/MA.py
+++ TrendAnalysis/MA.py
@@ -2,39 +2,34 @@ import pandas as pd
 from TechFunctions.StatisticalFunctions import SMA, EMA
 from TechFunctions.Technicalities import position
 
-class MACD:
+class MA:
     def __init__(self, 
                  data: pd.DataFrame, 
                  short_window: int = 10, 
                  long_window: int = 22,
-                 signal_window: int = 9,
                  exponential: bool = True,
                  alpha_EMA: float = 0.1):
         self.data = data
         self.short_window = short_window
         self.long_window = long_window
-        self.signal_window = signal_window
         self.alpha_EMA = alpha_EMA
         
         if exponential:
             self.applyfunction = lambda x: EMA(x, alpha_EMA)
         else:
             self.applyfunction = lambda x: SMA(x)
-     
-    def BaseLine(self) -> pd.DataFrame:
-        bl = self.data.rolling(self.short_window)\
-            .apply(self.applyfunction)\
-                - self.data.rolling(self.long_window)\
-                    .apply(self.applyfunction)
-        return bl
     
-    def SignalLine(self) -> pd.DataFrame:
-        bl = self.BaseLine()
-        sl = bl.rolling(self.signal_window).apply(self.applyfunction)
-        return sl
-
+    def Short(self):
+        short = self.data.apply(lambda x: x.rolling(self.short_window).apply(self.applyfunction))
+        return short
+    
+    def Long(self):
+        long = self.data.apply(lambda x: x.rolling(self.long_window).apply(self.applyfunction))
+        return long
+    
     def Decision(self):
-        UpwardTrend = self.BaseLine() > self.SignalLine()
+        UpwardTrend = (self.Short() > self.Long())
         decision = UpwardTrend.apply(lambda col:
-            col.rolling(2).apply(lambda x: position(x)))
+            col.rolling(2).apply(lambda x: position(x))
+        )
         return decision.fillna(0.0)
